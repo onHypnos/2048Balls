e23a33ec8857f42c4abcb7f66b1f28876d482d6e
Hypnos <47752595+Terryhata6@users.noreply.github.com>
2021-11-05 23:55:42 +0300

    Commit

--- Assets/Scripts/Controllers/LevelController.cs
+++ Assets/Scripts/Controllers/LevelController.cs
@@ -15,15 +15,20 @@ namespace Core
         [SerializeField] private SplineComputer _levelSpline;
         [SerializeField] private Transform _starterNode;
         [SerializeField] private List<BallView> _ballSnakeList;
-        [SerializeField] private Queue<BallView> _ballPool;
+        [SerializeField] private Queue<BallView> _ballPool = new Queue<BallView>();
         [SerializeField] private LineState _lineState;
-
+        [Header("Example")]
+        [SerializeField] private GameObject _ballExample;
         [Header("Properties")] [SerializeField]
         private List<int> _starterBalls;
 
+
+
+        private BallView _lastCalledBall;
+
         [SerializeField] [Range(0.1f, 2f)] private float _deployTime;
 
-        private void Start()
+        private void Awake()
         {
             if (_levelSpline == null)
             {
@@ -34,31 +39,55 @@ namespace Core
             {
                 Debug.LogWarning("StarterNode not setted");
             }
+
+            if (_ballPool == null)
+            {
+                _ballPool = new Queue<BallView>();
+                InitializePool<BallView>(_ballPool, _ballExample, 50, _starterNode.position + Vector3.right);
+            }
+
         }
 
-        private void InitializePool<T>(Queue<T> _list, GameObject example, int amount, Vector3 poolPosition)
+        private void InitializePool<T>(Queue<T> queue, GameObject example, int amount, Vector3 poolPosition)
         {
+            
+            if (queue == null)
+            {
+                queue = new Queue<T>();
+            }
+
             GameObject tempGO;
             for (int i = 0; i < amount; i++)
             {
-                tempGO = Instantiate(example, poolPosition, Quaternion.identity);
+                tempGO = Instantiate(example, poolPosition, Quaternion.identity, this.transform);
                 tempGO.SetActive(false);
-                _list.Enqueue(tempGO.GetComponent<T>());
+                queue.Enqueue(tempGO.GetComponent<T>());
             }
         }
 
-        public IEnumerator DeployStarterBalls(int ballAmount, List<int> ballsPower)
+        public void FindBackBall()
         {
-            var timeDelay = _deployTime / ballAmount;
-            for (int i = 0; i < ballAmount; i++)
+            double tempDistance = 1.01;
+            
+            for (int i = 0; i < _ballSnakeList.Count; i++)
             {
-                _tempBall = GetBallFromPool(ballsPower[i]);
-                yield return new WaitForSeconds(timeDelay);
+                if (_ballSnakeList[i].GetSplineProgressPercent() < tempDistance)
+                {
+                    tempDistance = _ballSnakeList[i].GetSplineProgressPercent();
+                    _tempBall = _ballSnakeList[i];
+                }
             }
+            _lastCalledBall = _tempBall;
         }
 
-        private void Update()
+        private void FixedUpdate()
         {
+            for (int i = 0; i < _ballSnakeList.Count; i++)
+            {
+                //_ballSnakeList[i].
+            }
+
+            
             switch (_lineState)
             {
                 case LineState.Await:
@@ -67,6 +96,7 @@ namespace Core
                 }
                 case LineState.Moving:
                 {
+                    _lastCalledBall.PushForward();
                     break;
                 }
                 case LineState.Regroup:
@@ -76,10 +106,32 @@ namespace Core
                 default: break;
             }
         }
-
+        
+        public IEnumerator DeployStarterBalls(int ballAmount, List<int> ballsPower)
+        {
+            var timeDelay = _deployTime / ballAmount;
+            for (int i = 0; i < ballAmount; i++)
+            {
+                _tempBall = GetBallFromPool(ballsPower[i]);
+                //_TempBall going to line
+                //_tempBall.SetSpline(_levelSpline);
+                SetBallOnSpline(_tempBall, _levelSpline);
+                _tempBall.transform.position = _starterNode.position;
+                _tempBall.gameObject.SetActive(true);
+                FindBackBall();
+                yield return new WaitForSeconds(timeDelay);
+            }
+            _lineState = LineState.Moving;
+        }
 
         private BallView GetBallFromPool(int ballPower)
         {
+            
+            if (_ballPool == null || _ballPool.Count == 0)
+            {
+                Debug.LogWarning("Pool is null"); //TODO разобраться с порядком вызова
+                InitializePool<BallView>(_ballPool, _ballExample, 50, _starterNode.position+Vector3.right);
+            }
             _tempBall = _ballPool.Dequeue();
             _tempBall.ChangeBallPower(ballPower);
             return _tempBall;
@@ -87,17 +139,21 @@ namespace Core
 
         private BallView SetBallOnSpline(BallView ball, SplineComputer spline)
         {
-            ball.SetPositionerSpline(spline);
+            ball.SetSpline(spline);
             if (!_ballSnakeList.Contains(ball))
             {
                 _ballSnakeList.Add(ball);
             }
+
+            
             return ball;
         }
 
 
         public void LevelStart()
         {
+            StartCoroutine(DeployStarterBalls(_starterBalls.Count, _starterBalls));
+            
             Debug.Log("Start level");
             GameEvents.Current.LevelStart();
         }
--- Assets/Scripts/Entities/Ball/BallView.cs
+++ Assets/Scripts/Entities/Ball/BallView.cs
@@ -1,25 +1,60 @@
 using System;
 using System.Collections.Generic;
 using Dreamteck.Splines;
+using TMPro;
 using UnityEngine;
 
 namespace Core
 {
     public class BallView : MonoBehaviour
     {
-        [SerializeField] private SplinePositioner _positioner;
+        [SerializeField] private SplineTracer _splineUser;
         [SerializeField] private int _ballPower;
         [SerializeField] private MeshRenderer _renderer;
         [SerializeField] private List<Material> _colorMaterials;
+        [SerializeField] private GameObject _textSphere;
+        [SerializeField] private List<TextMeshProUGUI> _textComponents;
+        private Rigidbody _rigidbody;
         
         
+        
+        
+        public Rigidbody RigidBody => _rigidbody;
+        
+        public void UpdateTextSpherePosition(Transform camera)
+        {
+            _textSphere.transform.LookAt(camera);
+        }
+
+        public void PushForward()
+        {
+            _rigidbody.velocity = _splineUser.result.forward*3f;
+        }
+
+        public double GetSplineProgressPercent()
+        {
+            return _splineUser.modifiedResult.percent;
+        }
+
+        private void Update()
+        {
+            transform.position = Vector3.Lerp(transform.position, _splineUser.result.position, 0.3f);
+            var magnitude = _rigidbody.velocity.magnitude;
+            _rigidbody.velocity = _splineUser.result.forward * magnitude*0.95f;
+        }
+
+
         public void ChangeBallPower(int power)
         {
             if (power < 10)
             {
                 if (_colorMaterials.Count < power)
                 {
-                    _renderer.sharedMaterial = _colorMaterials[power];
+                    //_renderer.sharedMaterial = _colorMaterials[power];
+                    for (int i = 0; i < _textComponents.Count; i++)
+                    {
+                        _textComponents[i].text = $"{Math.Pow(2, power)}";
+                    }
                 }
             }
             
@@ -29,11 +64,12 @@ namespace Core
         private void Awake()
         {
             gameObject.SetActive(false);
+            _rigidbody = GetComponent<Rigidbody>();
         }
 
-        public void SetPositionerSpline(SplineComputer spline)
+        public void SetSpline(SplineComputer spline)
         {
-            _positioner.spline = spline;
+            _splineUser.spline = spline;
 
         }
     }
